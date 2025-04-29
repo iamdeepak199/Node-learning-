087f180bd07e522b4e195bc6fdfda9c4453ba343
Deepak bhardwaj <80276722+iamdeepak199@users.noreply.github.com>
2025-04-29 11:53:09 +0530

    Create http server using Express

--- index.js
+++ index.js
@@ -1,7 +1,17 @@
 const {add,sub,multi,divide} = require ('./modules/mathfun');
 const chalk = require('chalk');
+const express = require('express');
+
+const app = express();
 
 console.log(chalk.blue.bold.inverse("Sum of two number is : ", add(4,2)));
 console.log(chalk.green.italic.inverse("Subtraction of two number is : " ,sub(4,2)));
 console.log(chalk.white.italic.inverse("Multiple of two number is : " ,multi(4,2)));
 console.log(chalk.red.bold.inverse("Division of two number is : " ,divide(4,2)));
+
+app.get("/",(req,res)=>{
+    res.send("hello From Express :");
+});
+app.listen(8000,()=>{
+    console.log("listing Port 8000");
+})
